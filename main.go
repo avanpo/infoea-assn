7526dcfcdbdcede66ef8a37339740d17f2a5ba47
alex <avanpoppelen@gmail.com>
2015-01-11 18:43:47 +0100

    Final experiment code

--- main.go
+++ main.go
@@ -4,11 +4,16 @@
 
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	//"time"
+	//"fmt"
+)
 
 const l = 100
 
 func main() {
+	// Initialize rand and random linkage
 	rand.Seed(1)
 	initRandomLink()
 
@@ -23,4 +28,18 @@ func main() {
 	// Experiment 3: uniform crossover, mutation
 	data3 := experiment(exp23FitFuncs, uniformCrossover, true)
 	writeData(3, data3)
+
+	// Test execution time for single algorithm run
+	/*start := time.Now()
+
+	p := fillPopulation(1280, tightDeceptiveTF, twoPointCrossover, false)
+	p = geneticAlgorithm(p, false)
+
+	elapsed := time.Since(start)
+
+	if containsOptimalSol(p) {
+		fmt.Printf("Found optimal solution in generation %d (%s)\n", p.t, elapsed)
+	} else {
+		fmt.Printf("Did not find optimal solution\n")
+	}*/
 }
